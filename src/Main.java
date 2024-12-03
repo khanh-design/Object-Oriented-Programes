3462f3d1b53bb04e24f809be17a44bb2657770af
khanh-design <buiduykhanh0162003@gmail.com>
2024-12-03 13:58:23 +0700

    OOP

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "HaiTT");
+        map.put(0, "Nguyen");
+        map.put(2, "Minh");
+        map.put(4, "Tai");
+        map.put(1, "Hai");
+//        Set<Integer> set = map.keySet();
+//        for (Integer i: set) {
+//            System.out.println(map.get(i));
+//        }
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
