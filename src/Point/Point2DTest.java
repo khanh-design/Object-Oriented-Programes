2cf1817335968c44820df8fb82b34eddbb33c36d
khanh-design <buiduykhanh0162003@gmail.com>
2024-12-07 19:50:01 +0700

    OOP

---
+++ src/Point/Point2DTest.java
@@ -0,0 +1,10 @@
+package Point;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D p = new Point2D();
+        p.setX(10);
+        p.setY(20);
+        System.out.println(p);
+    }
+}
---
+++ src/Point/Point3DTest.java
@@ -0,0 +1,8 @@
+package Point;
+
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D p1 = new Point3D(10,20,30);
+        System.out.println(p1);
+    }
+}
